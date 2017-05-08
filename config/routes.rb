424893082c664f3750b092bbb2d0f5dac6dd026e
Rodrigo Barreto <rodrigo.barreto@mall-connect.com>
2017-05-08 23:12:59 +0200

    route redirect

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'home/randomselect/:id' => 'home#randomselect', as: 'randomselect'
 
-  #get "*path" => redirect("/")
+  get "*path" => redirect("/")
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
